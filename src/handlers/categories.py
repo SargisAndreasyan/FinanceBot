20ae0765e5b1876d672f3cb4c8efcfa3a166ebb8
Sargis Andreasyan <andreeasyan.sargis@gmail.com>
2026-04-11 21:54:29 +0400

    Realized create_category<category_name> endpoint

--- src/handlers/categories.py
+++ src/handlers/categories.py
@@ -1,11 +1,24 @@
 from aiogram.filters import Command
 from aiogram import Router, types
+from services import CategoryService
 
 ctg = Router()
 
-@ctg.message(Command('create_category'))
+from aiogram.filters import Command
+from aiogram import types
+
+@ctg.message(Command("create_category"))
 async def create_category(message: types.Message):
-    print('Here will be created category')
+    args = message.text.split(maxsplit=1)
+    if len(args) < 2:
+        await message.answer("Usage: /create_category <name>")
+        return
+    category_name = args[1]
+    cs = CategoryService(message.from_user.id)
+    cs.create_category(category_name)
+
+    await message.answer(f"Category '{category_name}' created")
+
 
 @ctg.message(Command("remove_category"))
 async def remove_category(message: types.Message):
@@ -14,3 +27,4 @@ async def remove_category(message: types.Message):
 @ctg.message(Command("edit_category"))
 async def edit_category(message: types.Message):
     print('Here will be edited category')
+
--- src/services/category_service.py
+++ src/services/category_service.py
@@ -1,18 +1,15 @@
 from repositories.category_repo import CategoryRepository
 class CategoryService:
-
-    def __init__(self):
+    def __init__(self, user_id):
         self.repo = CategoryRepository()
+        self.user_id = user_id
 
-    def create_category(self, user_id, name) -> str:
+    def create_category(self, name) -> str:
         name = name.strip().lower()
-
-        if self.repo.get_by_name(user_id, name):
+        if self.repo.get_by_name(self.user_id, name):
             return f"{name} category already exists"
-
-        self.repo.create(user_id=user_id, name=name)
+        self.repo.create(user_id=self.user_id, name=name)
         return f"{name} category created successfully"
-
 
     def delete_category(self, name: str)->str:
         self.repo.delete(name=name)
