59a91f40f9d65c031fba822c5f4b44ad48faa26e
Sargis Andreasyan <andreeasyan.sargis@gmail.com>
2026-04-12 20:08:59 +0400

    Removed not used method

--- src/repositories/user_repo.py
+++ src/repositories/user_repo.py
@@ -7,22 +7,6 @@ from repositories.base import BaseRepository
 class UserRepository(BaseRepository):
     model = User
 
-    def create_user(self, t_id: int) -> Response:
-        db = SessionLocal()
-        try:
-            user = User(t_id=t_id)
-            db.add(user)
-            db.commit()
-            db.refresh(user)
-            return Response.ok(self._to_dict(user))
-
-        except Exception as e:
-            db.rollback()
-            return Response.fail(str(e))
-
-        finally:
-            db.close()
-
     def get_by_t_id(self, t_id: int) -> Response:
         db = SessionLocal()
         try:
